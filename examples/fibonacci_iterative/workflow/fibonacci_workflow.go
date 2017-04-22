629915de1bc3f746b9d97bf557d1b0539c1ca4fa
bradleyjkemp <bradleyjkemp@users.noreply.github.com>
2017-04-22 19:03:37 +0100

    Initial serial implementation (#1)
    Ugly initial implementation but does work

---
+++ examples/fibonacci_iterative/workflow/fibonacci_workflow.go
@@ -0,0 +1,84 @@
+package workflow
+
+import (
+	"github.com/bradleyjkemp/withtheflow"
+	"github.com/bradleyjkemp/withtheflow/examples/fibonacci_iterative/proto/fibonacci"
+	"github.com/bradleyjkemp/withtheflow/serial"
+	"github.com/golang/protobuf/proto"
+)
+
+const (
+	FibStep   = "fibStep"
+	InitStep  = "init"
+	PrintStep = "printStep"
+)
+
+func createFlowStep(index int64) withtheflow.FlowHandler {
+	return func(data []byte, flowHandle withtheflow.FlowHandle) ([]byte, error) {
+		fibData := &fibonacci.Data{}
+		if err := proto.Unmarshal(data, fibData); err != nil {
+			return nil, err
+		}
+
+		if fibData.Index == index {
+			resultData := &fibonacci.Result{
+				N:     fibData.N,
+				Index: fibData.Index,
+			}
+			result, _ := proto.Marshal(resultData)
+
+			flowHandle.NewFlow(withtheflow.FlowCall{PrintStep, result})
+
+			return result, nil
+		}
+
+		fibData.N, fibData.NMinus1 = fibData.N+fibData.NMinus1, fibData.N
+		fibData.Index++
+
+		nextStepData, _ := proto.Marshal(fibData)
+		flowHandle.NewFlow(withtheflow.FlowCall{FibStep, nextStepData})
+
+		return nil, nil
+	}
+}
+
+func initial(_ []byte, flowHandle withtheflow.FlowHandle) ([]byte, error) {
+	fibData := &fibonacci.Data{
+		NMinus1: 0,
+		N:       1,
+		Index:   1,
+	}
+
+	args, _ := proto.Marshal(fibData)
+
+	flowHandle.NewFlow(withtheflow.FlowCall{FibStep, args})
+
+	return nil, nil
+}
+
+func createReturnResult(dest *int64) func(result []byte, _ withtheflow.FlowHandle) ([]byte, error) {
+	return func(result []byte, _ withtheflow.FlowHandle) ([]byte, error) {
+		resultData := &fibonacci.Result{}
+		err := proto.Unmarshal(result, resultData)
+		if err != nil {
+			return nil, err
+		}
+
+		*dest = resultData.N
+
+		return nil, nil
+	}
+}
+
+func CreateWorkflow(index int64, dest *int64) withtheflow.Workflow {
+	flowHandlers := map[string](withtheflow.FlowHandler){
+		FibStep:   createFlowStep(index),
+		InitStep:  initial,
+		PrintStep: createReturnResult(dest),
+	}
+
+	workflow := serial.NewWorkflow(flowHandlers, nil)
+	workflow.NewFlow(withtheflow.FlowCall{InitStep, nil})
+
+	return workflow
+}
