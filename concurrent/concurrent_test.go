6648fdcb44f1eef0cb4ab5684c506bbb32e80917
bradleyjkemp <bradleyjkemp@users.noreply.github.com>
2017-05-05 22:26:11 +0000

    Work around deadlock bug

--- concurrent/concurrent_test.go
+++ concurrent/concurrent_test.go
@@ -1,6 +1,7 @@
 package concurrent_test
 
 import (
+	"github.com/bradleyjkemp/withtheflow"
 	"github.com/bradleyjkemp/withtheflow/concurrent"
 	"github.com/bradleyjkemp/withtheflow/examples/fibonacci"
 	"github.com/stretchr/testify/suite"
@@ -8,13 +9,13 @@ import (
 )
 
 func TestConcurrentFibonacci1Thread(t *testing.T) {
-	suite.Run(t, fibonacci.CreateFibonacciTestSuite(concurrent.NewRunner(1)))
+	suite.Run(t, fibonacci.CreateFibonacciTestSuite(func() withtheflow.WorkflowRunner { return concurrent.NewRunner(1) }))
 }
 
 func TestConcurrentFibonacci2Threads(t *testing.T) {
-	suite.Run(t, fibonacci.CreateFibonacciTestSuite(concurrent.NewRunner(2)))
+	suite.Run(t, fibonacci.CreateFibonacciTestSuite(func() withtheflow.WorkflowRunner { return concurrent.NewRunner(2) }))
 }
 
 func TestConcurrentFibonacci100Threads(t *testing.T) {
-	suite.Run(t, fibonacci.CreateFibonacciTestSuite(concurrent.NewRunner(100)))
+	suite.Run(t, fibonacci.CreateFibonacciTestSuite(func() withtheflow.WorkflowRunner { return concurrent.NewRunner(100) }))
 }
--- examples/fibonacci/fibonacci_test_suite.go
+++ examples/fibonacci/fibonacci_test_suite.go
@@ -9,28 +9,28 @@ import (
 
 type FibonacciTestSuite struct {
 	suite.Suite
-	runner withtheflow.WorkflowRunner
+	runner func() withtheflow.WorkflowRunner
 }
 
 func (s *FibonacciTestSuite) TestBaseCases() {
-	result := CalculateFibonacci(s.runner, 1)
+	result := CalculateFibonacci(s.runner(), 1)
 	assert.Equal(s.T(), big.NewInt(1), result)
 
-	result = CalculateFibonacci(s.runner, 2)
+	result = CalculateFibonacci(s.runner(), 2)
 	assert.Equal(s.T(), big.NewInt(1), result)
 }
 
 func (s *FibonacciTestSuite) TestIndex3() {
-	result := CalculateFibonacci(s.runner, 3)
+	result := CalculateFibonacci(s.runner(), 3)
 	assert.Equal(s.T(), big.NewInt(2), result)
 }
 
 func (s *FibonacciTestSuite) TestIndex10() {
-	result := CalculateFibonacci(s.runner, 10)
+	result := CalculateFibonacci(s.runner(), 10)
 	assert.Equal(s.T(), big.NewInt(55), result)
 }
 
-func CreateFibonacciTestSuite(runner withtheflow.WorkflowRunner) *FibonacciTestSuite {
+func CreateFibonacciTestSuite(runner func() withtheflow.WorkflowRunner) *FibonacciTestSuite {
 	testSuite := new(FibonacciTestSuite)
 	testSuite.runner = runner
 	return testSuite
